683f8d601af74d072effef3466a5d0a2e04b5319
yasumasa abe <abeyasumasa@MacBook-Air.local>
2019-09-07 17:32:06 +0900

    Fixed:コンフリクトを修正

--- config/application.rb
+++ config/application.rb
@@ -10,9 +10,5 @@ module EducationTask
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.generators do |g|
-      g.assets     false
-      g.helper     false
-    end
   end
 end
